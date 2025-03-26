1279f7c9221d01c7c6399c8c4c49282d73d8ed9e
khanh <khanh.nguyen.contact@gmail.com>
2025-03-26 23:56:18 +0800

    auto_push

--- pkg/fp/ast.go
+++ pkg/fp/ast.go
@@ -1,16 +1,5 @@
 package fp
 
-const (
-	BLOCKTYPE_NAME = "name" // name
-	BLOCKTYPE_EXPR = "expr" // name + list of blocks
-)
-
-type Block struct {
-	Type string
-	Name string
-	Args []*Block
-}
-
 // Expr : union of string, LambdaExpr
 type Expr interface{}
 type LambdaExpr struct {

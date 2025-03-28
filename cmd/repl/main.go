1af77b45b4a608890582be7f008efc40e77d4beb
khanh <khanh.nguyen.contact@gmail.com>
2025-03-29 00:41:40 +0800

    auto_push

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -16,9 +16,9 @@ func main() {
 	repl, welcome := repl.NewFP(fp.NewStdRuntime())
 	_, _ = fmt.Fprintf(os.Stderr, welcome)
 
-	// Create a readline instance
+	// Create a readline instance with a static prompt
 	rl, err := readline.NewEx(&readline.Config{
-		Prompt:          ">>>",                  // We manually print the prompt
+		Prompt:          ">>> ",                 // Default prompt
 		HistoryFile:     "/tmp/fp_repl_history", // Save command history
 		InterruptPrompt: "^C",
 	})
@@ -27,11 +27,14 @@ func main() {
 	}
 	defer rl.Close()
 
+	// Channel to signal interrupts (Ctrl+C)
 	interruptCh := make(chan struct{}, 1)
 
+	// Channel for OS signals (SIGINT, SIGTERM)
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
 
+	// Goroutine to listen for interrupts and notify REPL
 	go func() {
 		for sig := range signalCh {
 			if sig == os.Interrupt {
@@ -46,6 +49,7 @@ func main() {
 	}()
 
 	for {
+		// Read input
 		line, err := rl.Readline()
 		if err != nil {
 			if errors.Is(err, readline.ErrInterrupt) { // Handle Ctrl+C
@@ -58,12 +62,21 @@ func main() {
 			panic(err)
 		}
 
+		// Process input in REPL
 		output, executed := repl.ReplyInput(line, interruptCh)
 
+		// Print REPL output
 		if output != "" {
 			_, _ = fmt.Fprint(os.Stderr, output)
 		}
-		_ = executed
 
+		// If executed is true, print prompt again
+		if executed {
+			// Reset the prompt to ">>> " when input is executed
+			rl.SetPrompt(">>> ")
+		} else {
+			// Otherwise, indent continuation line (you can choose what to show)
+			rl.SetPrompt("    ") // Or set it to "" for no prompt if not executed
+		}
 	}
 }
